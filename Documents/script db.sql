6f4f288c28baa005659cabef2205f21717f7ff3e
Niverline <73484198+niver26@users.noreply.github.com>
2020-12-03 00:24:19 +0100

    Update script db.sql

--- Documents/script db.sql
+++ Documents/script db.sql
@@ -3,7 +3,8 @@ CREATE TABLE Utilisateur(
     IdUtilisateur NUMBER(10)NOT NULL,
     Pseudo VARCHAR2(45) NOT NULL,
     Email VARCHAR2(100) NOT NULL,
-    DateInscriptionDATE NOT NULL,
+    password VARCHAR2(100) NOT NULL,
+    DateInscription DATE NOT NULL,
     PRIMARY KEY (IdUtilisateur)
 );
 
@@ -16,7 +17,7 @@ CREATE TABLE Ingredient(
 CREATE TABLE Images(
     IdImages NUMBER(10)NOT NULL,
     UrlImages VARCHAR2(200),
-    PRIMARY KEY (idImage)
+    PRIMARY KEY (IdImages)
 );
 
 CREATE TABLE Recette(
@@ -29,10 +30,10 @@ CREATE TABLE Recette(
     Coût VARCHAR2(45) NOT NULL,
     IdUtilisateur NUMBER(10) NOT NULL,
     idImages NUMBER(10) NOT NULL,
-    PRIMARY KEY (IDRecette),
+    PRIMARY KEY (IdRecette),
     CONSTRAINT fk_Recette_Utilisateur
         FOREIGN KEY (idUtilisateur)
-        REFERENCES Utilisateur(idUtilisateur),
+        REFERENCES Utilisateur(IdUtilisateur),
     CONSTRAINT fk_Recette_Image
         FOREIGN KEY (idImage)
         REFERENCES Images(IdImages)
@@ -61,7 +62,7 @@ CREATE TABLE Etape(
     PRIMARY KEY (idEtape),
     CONSTRAINT fk_Etape_Recette
         FOREIGN KEY(idRecette)
-        REFERENCES Recette(idRecette)
+        REFERENCES Recette(IdRecette)
 );
 
 CREATE TABLE IngredientRecette(
@@ -69,14 +70,14 @@ CREATE TABLE IngredientRecette(
     Quantité float(10) NOT NULL,
     Mesure VARCHAR2(30) NOT NULL,
     idRecette NUMBER(10) NOT NULL,
-    IdIngredient NUMBER(10)NOT NULL,
+    idIngredient NUMBER(10)NOT NULL,
     PRIMARY KEY(idIngredientRecette),
     CONSTRAINT fk_IngredientRecette_Ingredient
-        FOREIGN KEY(IdIngredient)
+        FOREIGN KEY(idIngredient)
         REFERENCES Ingredient(IdIngredient),
     CONSTRAINT fk_IngredientRecette_Recette
         FOREIGN KEY(idRecette)
-        REFERENCES Recette(idRecette)
+        REFERENCES Recette(IdRecette)
 );
 
 ------------ Script gestion ID -----------------
@@ -90,4 +91,85 @@ BEGIN
     
 END;
 /
+/********************* ajout id autoincrement **************************/
+CREATE TRIGGER ajout_IDutilisateur BEFORE INSERT
+ ON Utilisateur 
+ FOR EACH ROW
+ BEGIN
+	DECLARE IdIncremente int;
+	IF (NEW.idUtilisateur IS NULL) THEN
+		SET IdIncremente = (SELECT MAX(idUtilisateur)+1 FROM Utilisateur ) ;
+		SET NEW.idUtilisateur = IdIncremente ;
+	END IF;
+ END 
+ 
+ 
+ CREATE TRIGGER ajout_IdIngredient BEFORE INSERT
+ ON Ingredient 
+ FOR EACH ROW
+ BEGIN
+	DECLARE IdIncremente int;
+	IF (NEW.IdIngredient IS NULL) THEN
+		SET IdIncremente = (SELECT MAX(IdIngredient)+1 FROM Ingredient ) ;
+		SET NEW.IdIngredient = IdIncremente ;
+	END IF;
+ END 
+ 
+ CREATE TRIGGER ajout_IdImages BEFORE INSERT
+ ON Images
+ FOR EACH ROW
+ BEGIN
+	DECLARE IdIncremente int;
+	IF (NEW.IdImages IS NULL) THEN
+		SET IdIncremente = (SELECT MAX(IdImages)+1 FROM Images ) ;
+		SET NEW.IdImages = IdIncremente ;
+	END IF;
+ END 
+ 
+ CREATE TRIGGER ajout_IdRecette BEFORE INSERT
+ ON Recette
+ FOR EACH ROW
+ BEGIN
+	DECLARE IdIncremente int;
+	IF (NEW.IdRecette IS NULL) THEN
+		SET IdIncremente = (SELECT MAX(IdRecette)+1 FROM Recette ) ;
+		SET NEW.IdRecette = IdIncremente ;
+	END IF;
+ END 
+ 
+ 
+ CREATE TRIGGER ajout_IdCommentaire BEFORE INSERT
+ ON Commentaire
+ FOR EACH ROW
+ BEGIN
+	DECLARE IdIncremente int;
+	IF (NEW.IdCommentaire IS NULL) THEN
+		SET IdIncremente = (SELECT MAX(IdCommentaire)+1 FROM Commentaire ) ;
+		SET NEW.IdCommentaire = IdIncremente ;
+	END IF;
+ END 
+ 
+ CREATE TRIGGER ajout_idEtape BEFORE INSERT
+ ON Etape
+ FOR EACH ROW
+ BEGIN
+	DECLARE IdIncremente int;
+	IF (NEW.idEtape IS NULL) THEN
+		SET IdIncremente = (SELECT MAX(idEtape)+1 FROM Etape ) ;
+		SET NEW.idEtape = IdIncremente ;
+	END IF;
+ END 
+ 
+
+ CREATE TRIGGER ajout_idIngredientRecette BEFORE INSERT
+ ON IngredientRecette
+ FOR EACH ROW
+ BEGIN
+	DECLARE IdIncremente int;
+	IF (NEW.idIngredientRecette IS NULL) THEN
+		SET IdIncremente = (SELECT MAX(idIngredientRecette)+1 FROM IngredientRecette ) ;
+		SET NEW.idIngredientRecette = IdIncremente ;
+	END IF;
+ END 
+ 
 ----------- Script population des tables --------------
